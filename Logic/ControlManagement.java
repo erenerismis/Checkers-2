7635e4fcfbb985945c947be39bedd815d6be9bd7
Kaan <kaan.ayaydin@ozu.edu.tr>
2024-12-21 13:22:11 +0300

    Some of the game logic methods is added.

--- Logic/ControlManagement.java
+++ Logic/ControlManagement.java
@@ -1,4 +1,37 @@
 package Logic;
 
 public class ControlManagement {
+    private Board board;
+    private Player player1;
+    private Player player2;
+    private GameRules gameRules;
+
+    public ControlManagement(Board board, Player player1, Player player2, GameRules gameRules) {
+        this.board = board;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.gameRules = gameRules;
+    }
+
+    public boolean isMoveValid(int startX, int startY, int endX, int endY) {
+        return gameRules.isValidMove(board.getBoard(), startX, startY, endX, endY);
+    }
+
+    public void makeMove(int startX, int startY, int endX, int endY) {
+        gameRules.makeMove(board.getBoard(), startX, startY, endX, endY);
+    }
+
+    public Player getCurrentPlayer() {
+        return player1.isTurn() ? player1 : player2;
+    }
+
+    public void switchTurn() {
+        player1.setTurn(!player1.isTurn());
+        player2.setTurn(!player2.isTurn());
+    }
+
+    public boolean isGameOver() {
+        return (player1.getCapturedPieces() == (board.getBoardSize() * board.getBoardSize()) / 2) ||
+                (player2.getCapturedPieces() == (board.getBoardSize() * board.getBoardSize()) / 2);
+    }
 }
--- Logic/GameRules.java
+++ Logic/GameRules.java
@@ -1,4 +1,67 @@
 package Logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameRules {
+
+    private int boardSize;
+
+    public GameRules(int boardSize) {
+        this.boardSize = boardSize;
+    }
+
+    public boolean isValidMove(int[][] board, int startX, int startY, int endX, int endY) {
+        if (endX < 0 || endX >= boardSize || endY < 0 || endY >= boardSize || board[endX][endY] != 0) {
+            return false;
+        }
+
+        int piece = board[startX][startY];
+        int dx = endX - startX;
+        int dy = endY - startY;
+
+        if (Math.abs(dx) == 1 && Math.abs(dy) == 1) {
+            return (piece == 1 && dx == -1) || (piece == 2 && dx == 1) || isKing(piece);
+        }
+
+        if (Math.abs(dx) == 2 && Math.abs(dy) == 2) {
+            int midX = startX + dx / 2;
+            int midY = startY + dy / 2;
+            return isCaptureMove(board, startX, startY, endX, endY, midX, midY);
+        }
+
+        return false;
+    }
+
+    public boolean isCaptureMove(int[][] board, int startX, int startY, int endX, int endY, int midX, int midY) {
+        int piece = board[startX][startY];
+        int opponent = (piece == 1) ? 2 : 1;
+        return board[midX][midY] == opponent && board[endX][endY] == 0;
+    }
+
+    public void makeMove(int[][] board, int startX, int startY, int endX, int endY) {
+        int piece = board[startX][startY];
+        board[startX][startY] = 0;
+        board[endX][endY] = piece;
+
+        if (Math.abs(startX - endX) == 2) {
+            int midX = (startX + endX) / 2;
+            int midY = (startY + endY) / 2;
+            board[midX][midY] = 0;
+        }
+
+        if (isKingPromotion(piece, endX)) {
+            board[endX][endY] = piece + 2;
+        }
+    }
+
+    public boolean isKingPromotion(int piece, int x) {
+        return (piece == 1 && x == 0) || (piece == 2 && x == boardSize - 1);
+    }
+
+    public boolean isKing(int piece) {
+        return piece == 3 || piece == 4;
+    }
+
+
 }
