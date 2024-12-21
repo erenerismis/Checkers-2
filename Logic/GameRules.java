87907a79d1586ce58c77905edb10c6c017a8b408
Kaan <kaan.ayaydin@ozu.edu.tr>
2024-12-21 21:44:14 +0300

    Some of the GameRules methods are rearranged and new methods are added.

--- Logic/GameRules.java
+++ Logic/GameRules.java
@@ -5,63 +5,123 @@ import java.util.List;
 
 public class GameRules {
 
-    private int boardSize;
+    public class GameRules {
 
-    public GameRules(int boardSize) {
-        this.boardSize = boardSize;
-    }
+        public boolean isValidMove(Board board, int startX, int startY, int endX, int endY) {
+            Piece piece = board.getPieceAt(startX, startY);
+            if (piece == null) return false;
 
-    public boolean isValidMove(int[][] board, int startX, int startY, int endX, int endY) {
-        if (endX < 0 || endX >= boardSize || endY < 0 || endY >= boardSize || board[endX][endY] != 0) {
+            int deltaX = Math.abs(endX - startX);
+            int deltaY = Math.abs(endY - startY);
+
+            if (piece.isKing()) {
+                if (deltaX == deltaY) {
+                    int dirX = (endX - startX) > 0 ? 1 : -1;
+                    int dirY = (endY - startY) > 0 ? 1 : -1;
+                    int currentX = startX + dirX;
+                    int currentY = startY + dirY;
+
+                    boolean enemyFound = false;
+
+                    while (currentX != endX && currentY != endY) {
+                        Piece currentPiece = board.getPieceAt(currentX, currentY);
+                        if (currentPiece != null) {
+                            if (enemyFound || currentPiece.getColor().equals(piece.getColor())) {
+                                return false;
+                            }
+                            enemyFound = true;
+                        }
+                        currentX += dirX;
+                        currentY += dirY;
+                    }
+
+                    if (enemyFound && board.getPieceAt(endX, endY) == null) {
+                        return true;
+                    }
+
+                    return !enemyFound && board.getPieceAt(endX, endY) == null;
+                }
+            }
+
+            if ((piece.getColor().equals("White") && startX > endX) ||
+                    (piece.getColor().equals("Black") && startX < endX)) {
+                if (deltaX == 1 && deltaY == 1) {
+                    return board.getPieceAt(endX, endY) == null;
+                } else if (deltaX == 2 && deltaY == 2) {
+                    int midX = (startX + endX) / 2;
+                    int midY = (startY + endY) / 2;
+                    Piece midPiece = board.getPieceAt(midX, midY);
+                    return midPiece != null && !midPiece.getColor().equals(piece.getColor()) &&
+                            board.getPieceAt(endX, endY) == null;
+                }
+            }
+
             return false;
         }
 
-        int piece = board[startX][startY];
-        int dx = endX - startX;
-        int dy = endY - startY;
+        public boolean hasMandatoryCapture(Board board, String playerColor) {
+            int size = board.getSize();
+            boolean captureFound = false;
 
-        if (Math.abs(dx) == 1 && Math.abs(dy) == 1) {
-            return (piece == 1 && dx == -1) || (piece == 2 && dx == 1) || isKing(piece);
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    Piece piece = board.getPieceAt(i, j);
+
+                    if (piece != null && piece.getColor().equals(playerColor)) {
+                        if (canCapture(board, i, j)) {
+                            System.out.println("DEBUG: Piece at (" + i + ", " + j + ") can capture.");
+                            captureFound = true;
+                        }
+                    }
+                }
+            }
+
+            if (!captureFound) {
+                System.out.println("DEBUG: No mandatory captures for " + playerColor);
+            }
+
+            return captureFound;
         }
 
-        if (Math.abs(dx) == 2 && Math.abs(dy) == 2) {
-            int midX = startX + dx / 2;
-            int midY = startY + dy / 2;
-            return isCaptureMove(board, startX, startY, endX, endY, midX, midY);
+        public boolean canCapture(Board board, int startX, int startY) {
+            Piece piece = board.getPieceAt(startX, startY);
+            if (piece == null) return false;
+
+            int[][] directions;
+
+            if (piece.isKing()) {
+                directions = new int[][] {{-2, -2}, {-2, 2}, {2, -2}, {2, 2}};
+            } else if (piece.getColor().equals("White")) {
+                directions = new int[][] {{-2, -2}, {-2, 2}};
+            } else {
+                directions = new int[][] {{2, -2}, {2, 2}};
+            }
+
+            for (int[] dir : directions) {
+                int midX = startX + dir[0] / 2;
+                int midY = startY + dir[1] / 2;
+                int endX = startX + dir[0];
+                int endY = startY + dir[1];
+
+                if (endX >= 0 && endX < board.getSize() && endY >= 0 && endY < board.getSize()) {
+                    Piece midPiece = board.getPieceAt(midX, midY);
+                    if (midPiece != null && !midPiece.getColor().equals(piece.getColor()) &&
+                            board.getPieceAt(endX, endY) == null) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
 
-        return false;
-    }
-
-    public boolean isCaptureMove(int[][] board, int startX, int startY, int endX, int endY, int midX, int midY) {
-        int piece = board[startX][startY];
-        int opponent = (piece == 1) ? 2 : 1;
-        return board[midX][midY] == opponent && board[endX][endY] == 0;
-    }
-
-    public void makeMove(int[][] board, int startX, int startY, int endX, int endY) {
-        int piece = board[startX][startY];
-        board[startX][startY] = 0;
-        board[endX][endY] = piece;
-
-        if (Math.abs(startX - endX) == 2) {
-            int midX = (startX + endX) / 2;
-            int midY = (startY + endY) / 2;
-            board[midX][midY] = 0;
+        public boolean canCaptureAgain(Board board, int startX, int startY) {
+            return canCapture(board, startX, startY);
         }
 
-        if (isKingPromotion(piece, endX)) {
-            board[endX][endY] = piece + 2;
+        public boolean shouldPromote(Piece piece, int endX, int boardSize) {
+            return (!piece.isKing() && ((piece.getColor().equals("White") && endX == 0) ||
+                    (piece.getColor().equals("Black") && endX == boardSize - 1)));
         }
     }
 
-    public boolean isKingPromotion(int piece, int x) {
-        return (piece == 1 && x == 0) || (piece == 2 && x == boardSize - 1);
-    }
-
-    public boolean isKing(int piece) {
-        return piece == 3 || piece == 4;
-    }
-
-
-}
