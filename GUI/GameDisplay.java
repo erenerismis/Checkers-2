98121328cc4ab56fc8897d02da9a5b51be6a3ce4
ozers9997 <ozer.sevinc@ozu.edu.tr>
2024-12-23 00:39:54 +0300

    playerinfowindow added to the MainGUI and adjustments made settled for GUI.

--- GUI/GameDisplay.java
+++ GUI/GameDisplay.java
@@ -2,34 +2,58 @@ package GUI;
 
 import Game.Board;
 import Game.Piece;
+import Game.Player;
 
 import javax.swing.*;
 import java.awt.*;
 
+
+
 public class GameDisplay extends JFrame {
     private Board board;
+    private Player player1;
     private final int TILE_SIZE = 80;
+    private Player player2;
+    private JPanel boardPanel;
+    private PlayerInfoWindow playerInfoWindow;
     private String player1Name;
     private String player2Name;
+    private PlayerTimer player1Timer;
+    private PlayerTimer player2Timer;
     private int timeLimit;
 
-    public GameDisplay(Board board, String player1Name, String player2Name, int timeLimit) {
+
+    public GameDisplay(Board board, Player player1, Player player2, int timeLimit) {
         this.board = board;
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-        this.timeLimit = timeLimit;
 
-        setTitle("Checkers 2");
-        setSize(board.getSize() * TILE_SIZE, board.getSize() * TILE_SIZE + 50);
+
+        setTitle("Checkers Game");
+        setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
-        setLocationRelativeTo(null);
-    }
+        setLayout(new BorderLayout());
 
+
+        boardPanel = createBoardPanel();
+        add(boardPanel, BorderLayout.CENTER);
+
+
+        playerInfoWindow = new PlayerInfoWindow(player1, player2);
+
+        JButton showPlayerInfoButton = new JButton("Show Player Info");
+        showPlayerInfoButton.addActionListener(e -> playerInfoWindow.setVisible(true));
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.add(showPlayerInfoButton);
+        add(bottomPanel, BorderLayout.SOUTH);
+    }
 
     public void render() {
         JPanel boardPanel = new JPanel(new GridLayout(board.getSize(), board.getSize()));
         boardPanel.setPreferredSize(new Dimension(board.getSize() * TILE_SIZE, board.getSize() * TILE_SIZE));
+        JPanel bottomPanel = new JPanel();
+        JButton showPlayerInfoButton = new JButton("Show Player Info");
+        showPlayerInfoButton.addActionListener(e -> playerInfoWindow.setVisible(true));
+        bottomPanel.add(showPlayerInfoButton);
 
         for (int i = 0; i < board.getSize(); i++) {
             for (int j = 0; j < board.getSize(); j++) {
@@ -58,9 +82,11 @@ public class GameDisplay extends JFrame {
 
 
         getContentPane().removeAll();
+
         add(boardPanel);
-        revalidate();
-        repaint();
+        add(bottomPanel, BorderLayout.SOUTH);
+        boardPanel.revalidate();
+        boardPanel.repaint();
     }
 
 
@@ -68,5 +94,37 @@ public class GameDisplay extends JFrame {
         String currentPlayer = isWhiteTurn ? player1Name : player2Name;
         setTitle("Checkers - Turn: " + currentPlayer + " - Time: " + timeLimit + " min");
     }
+
+
+    private JPanel createBoardPanel() {
+        JPanel panel = new JPanel(new GridLayout(board.getSize(), board.getSize()));
+
+        for (int i = 0; i < board.getSize(); i++) {
+            for (int j = 0; j < board.getSize(); j++) {
+                JPanel tile = new JPanel();
+                tile.setPreferredSize(new Dimension(80, 80));
+
+                if ((i + j) % 2 == 0) {
+                    tile.setBackground(Color.LIGHT_GRAY);
+                } else {
+                    tile.setBackground(Color.DARK_GRAY);
+
+                    Piece piece = board.getPieceAt(i, j);
+                    if (piece != null) {
+                        JLabel pieceLabel = new JLabel(piece.isKing() ? "K" : "P");
+                        pieceLabel.setForeground(piece.getColor().equals("White") ? Color.WHITE : Color.BLACK);
+                        pieceLabel.setFont(new Font("Arial", Font.BOLD, 30));
+                        pieceLabel.setHorizontalAlignment(SwingConstants.CENTER);
+                        tile.add(pieceLabel);
+                    }
+                }
+
+                panel.add(tile);
+            }
+        }
+
+        return panel;
+    }
+
 
 }
